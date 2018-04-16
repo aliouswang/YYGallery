bdddea0b5167c0d30834173b81c8ddb8dc03e9a7
aliouswang <wsw622@126.com>
2018-04-16 18:15:14 +0800

    fixed issue-101

--- app/src/main/java/com/hmzl/aliouswang/yygallery/ImageGalleryActivity.java
+++ app/src/main/java/com/hmzl/aliouswang/yygallery/ImageGalleryActivity.java
@@ -15,4 +15,10 @@ public class ImageGalleryActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //fixed issue 101
+    }
 }
